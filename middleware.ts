b419e759774ef793d9077a87b75dd863eec2a064
Rishi2600 <rishiraj6177@gmail.com>
2025-05-16 23:27:05 +0530

    middleware for access based on role based auth

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,17 @@
-export {default} from "next-auth/middleware"
+import withAuth from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+    function middleware(req) {
+        console.log(req.nextUrl.pathname)
+        console.log(req.nextauth.token.role)
+
+        if(req.nextUrl.pathname.startsWith("/Admin") && req.nextauth.token.role != "admin") {
+            return NextResponse.rewrite(new URL("/Denied", req.url))
+        }
+    },
+    callbacks: 
+)
 
 export const config = {
     matcher: [
