fa3a54cdaa451af1b19d3f7145b58cfc8ad8c67f
Rishi2600 <rishiraj6177@gmail.com>
2025-05-17 10:15:40 +0530

    user form created

---
+++ app/(components)/userform.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const useForm = () => {
+  const router = useRouter();
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setErrorMessage("");
+
+    const res = await fetch("/api/Users", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      //@ts-ignore
+      "content-type": "application/json",
+    });
+
+    if (!res.ok) {
+      const response = await res.json();
+      setErrorMessage(response.message);
+    } else {
+      router.refresh();
+      router.push("/");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} method="post" className="">
+        <h1>Create New User</h1>
+        <label>Full Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.name}
+          className=""
+        />
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.email}
+          className=""
+        />
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          required={true}
+          value={formData.password}
+          className=""
+        />
+        <input
+          type="submit"
+          value="create-user"
+          className=""
+        />
+      </form>
+      <p>{errorMessage}</p>
+    </>
+  );
+};
+
+export default useForm;
